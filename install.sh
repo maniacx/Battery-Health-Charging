10c661914c62102e8584e388d417ff9368ee548f
maniacx <macs18max@gmail.com>
2023-02-03 10:33:11 -0600

    Bash Installer: Clean up
    Remove leftovers

--- install.sh
+++ install.sh
@@ -9,10 +9,9 @@ gnome-extensions pack ./ \
     --extra-source=resources/ \
     --extra-source=driver.js \
     --extra-source=preference.ui \
-    --extra-source=LICENSE.md \
     --force \
 
 echo "Installing extension..."
 gnome-extensions install Battery-Health-Charging@maniacx.github.com.shell-extension.zip --force
 
-echo "Extension installed Battery Health Charging Limit succesfully. Restart the shell (or logout) to be able to enable the extension."
+echo "Extension installed Battery Health Charging succesfully. Restart the shell (or logout) to be able to enable the extension."
