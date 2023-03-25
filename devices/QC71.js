c0e0956640fd8a4d62d04898bb5c02fc608ad363
maniacx <macs18max@gmail.com>
2023-03-24 20:19:38 -0700

    Battery Health Extesnsion: Add QC71 devices. Dependencies https://github.com/pobrn/qc71_laptop Supports selected model from TUXEDO, XMG, Eluktronics

---
+++ devices/QC71.js
@@ -0,0 +1,56 @@
+'use strict';
+/* Generic and Intel QC71 device Laptops with BAT0. For QC71 device dependencies https://github.com/pobrn/qc71_laptop */
+const {GObject} = imports.gi;
+const ExtensionUtils = imports.misc.extensionUtils;
+const Me = ExtensionUtils.getCurrentExtension();
+const Helper = Me.imports.lib.helper;
+const {fileExists, readFileInt, runCommandCtl} = Helper;
+
+const VENDOR_QC71 = '/sys/devices/platform/qc71_laptop';
+const BAT0_END_PATH = '/sys/class/power_supply/BAT0/charge_control_end_threshold';
+
+
+var QC71SingleBatteryBAT0 = GObject.registerClass({
+    Signals: {'read-completed': {}},
+}, class QC71SingleBatteryBAT0 extends GObject.Object {
+    name = 'QC71 with Single Battery BAT0';
+    type = 24;
+    deviceNeedRootPermission = true;
+    deviceHaveDualBattery = false;
+    deviceHaveStartThreshold = false;
+    deviceHaveVariableThreshold = true;
+    deviceHaveBalancedMode = true;
+    iconForFullCapMode = 'ful100';
+    iconForBalanceMode = 'bal080';
+    iconForMaxLifeMode = 'max060';
+
+    isAvailable() {
+        if (!fileExists(VENDOR_QC71))
+            return false;
+        if (!fileExists(BAT0_END_PATH))
+            return false;
+        return true;
+    }
+
+    async setThresholdLimit(chargingMode) {
+        let returnError = false;
+        const settings = ExtensionUtils.getSettings();
+        const endValue = settings.get_int(`current-${chargingMode}-end-threshold`);
+        let status = await runCommandCtl('BAT0_END', `${endValue}`, null, false);
+        if (status === 0)  {
+            this.endLimitValue = readFileInt(BAT0_END_PATH);
+            if (endValue === this.endLimitValue)
+                this.emit('read-completed');
+            else
+                returnError = true;
+        } else {
+            returnError = true;
+        }
+        if (returnError) {
+            log('Battery Health Charging: Error threshold values not updated');
+            status = 1;
+        }
+        return status;
+    }
+});
+
