f623c341c6dcb8d87d94ff305d3d1f7fc29d3174
maniacx <macs18max@gmail.com>
2023-03-26 13:04:33 -0700

    Battery Health Charging: Use one fixed window size.
    There is no perfect windows size with different resolution, scaling and theming.

--- prefs.js
+++ prefs.js
@@ -1,7 +1,6 @@
 'use strict';
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
-const Config = imports.misc.config;
 const Driver = Me.imports.lib.driver;
 
 const {General} = Me.imports.preferences.general;
@@ -9,16 +8,13 @@ const {ThresholdPrimary} = Me.imports.preferences.thresholdPrimary;
 const {ThresholdSecondary} = Me.imports.preferences.thresholdSecondary;
 const {addMenu} = Me.imports.preferences.menu;
 
-const [major] = Config.PACKAGE_VERSION.split('.');
-
 function fillPreferencesWindow(window) {
     const settings = ExtensionUtils.getSettings();
 
     if (Driver.currentDevice === null)
         Driver.checkCompatibility();
 
-    const windowHeight = Number.parseInt(major) === 43 ? 700 : 775;
-    window.set_default_size(650, windowHeight);
+    window.set_default_size(650, 700);
 
     addMenu(window);
 
