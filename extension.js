cdc29f4e124392e25d4135250863a920ca45412e
maniacx <macs18max@gmail.com>
2023-03-31 15:12:41 -0700

    Battery health Charging: Correct Battery status icon in system tray
    When charging threshold is set, Upower reports Device.state as either charging,
    charge-pending or discharging depending on the battery level.
    
    This displays the battery Status icon sometimes to on-battery even though charger is always plugged-in.
    
    There is no way of knowing that charger is powered-off / disconnected, if we miss the notification sound
    
    We will read powersupply AC/ADP online status and change the battery icon accordingly,
    So that when charger is plugged (regardless of battery not charging due the charging threshold set) battery icon will display as charging/plugged-in.

--- extension.js
+++ extension.js
@@ -1,9 +1,12 @@
 'use strict';
+const Main = imports.ui.main;
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
 const Panel = Me.imports.lib.thresholdPanel;
+const PowerIcon = Me.imports.lib.powerIcon;
 
 var thresholdPanel = null;
+var powerIcon = null;
 
 function init() {
     ExtensionUtils.initTranslations(Me.metadata.uuid);
@@ -11,9 +14,18 @@ function init() {
 
 function enable() {
     thresholdPanel = new Panel.ThresholdPanel();
+    if (powerIcon === null)
+        powerIcon = new PowerIcon.BatteryStatusIndicator();
 }
 
 function disable() {
     thresholdPanel.destroy();
     thresholdPanel = null;
+
+    // Battery indicator icon in System-tray is also used in lockscreen
+    // Do not disable powerIcon in lockscreen
+    if (!Main.sessionMode.isLocked) {
+        powerIcon.disable();
+        powerIcon = null;
+    }
 }
