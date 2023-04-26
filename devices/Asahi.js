423a75cef487a5543ee3e88195065769f44d8e09
maniacx <macs18max@gmail.com>
2023-04-26 13:15:41 -0400

    Battery Health Charging: Add support for AsahiLinux Apple Macbook (M processors)

--- devices/Asahi.js
+++ devices/Asahi.js
@@ -1,22 +1,22 @@
 'use strict';
-/* Apple Mac book with Intel processors. install dkms https://github.com/c---/applesmc-next  */
+/* Apple Mac book with M-series processor - Asahi linux Laptop https://asahilinux.org/ */
 const {GObject} = imports.gi;
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
 const Helper = Me.imports.lib.helper;
 const {fileExists, readFileInt, runCommandCtl} = Helper;
 
-const VENDOR_APPLE = '/sys/module/applesmc';
-const BAT0_END_PATH = '/sys/class/power_supply/BAT0/charge_control_end_threshold';
+const ASAHI_END_PATH = '/sys/class/power_supply/macsmc-battery/charge_control_end_threshold';
+const ASAHI_START_PATH = '/sys/class/power_supply/macsmc-battery/charge_control_start_threshold';
 
-var AppleSingleBattery = GObject.registerClass({
+var AsahiSingleBattery = GObject.registerClass({
     Signals: {'read-completed': {}},
-}, class AppleSingleBattery extends GObject.Object {
-    name = 'Apple with Single Battery';
-    type = 16;
+}, class AsahiSingleBattery extends GObject.Object {
+    name = 'AppleAsahiLinux with Single Battery';
+    type = 25;
     deviceNeedRootPermission = true;
     deviceHaveDualBattery = false;
-    deviceHaveStartThreshold = false;
+    deviceHaveStartThreshold = true;
     deviceHaveVariableThreshold = true;
     deviceHaveBalancedMode = true;
     deviceHaveAdaptiveMode = false;
@@ -29,12 +29,19 @@ var AppleSingleBattery = GObject.registerClass({
     endBalancedRangeMax = 85;
     endBalancedRangeMin = 65;
     endMaxLifeSpanRangeMax = 85;
-    endMaxLifeSpanRangeMin = 50;
+    endMaxLifeSpanRangeMin = 52;
+    startFullCapacityRangeMax = 98;
+    startFullCapacityRangeMin = 75;
+    startBalancedRangeMax = 83;
+    startBalancedRangeMin = 60;
+    startMaxLifeSpanRangeMax = 83;
+    startMaxLifeSpanRangeMin = 50;
+    minDiffLimit = 2;
 
     isAvailable() {
-        if (!fileExists(VENDOR_APPLE))
+        if (!fileExists(ASAHI_END_PATH))
             return false;
-        if (!fileExists(BAT0_END_PATH))
+        if (!fileExists(ASAHI_START_PATH))
             return false;
         return true;
     }
@@ -43,10 +50,12 @@ var AppleSingleBattery = GObject.registerClass({
         let returnError = false;
         const settings = ExtensionUtils.getSettings();
         const endValue = settings.get_int(`current-${chargingMode}-end-threshold`);
-        let status = await runCommandCtl('BAT0_END', `${endValue}`, null, false);
+        const startValue = settings.get_int(`current-${chargingMode}-start-threshold`);
+        let status = await runCommandCtl('ASAHI_END_START', `${endValue}`, `${startValue}`, false);
         if (status === 0)  {
-            this.endLimitValue = readFileInt(BAT0_END_PATH);
-            if (endValue === this.endLimitValue)
+            this.endLimitValue = readFileInt(ASAHI_END_PATH);
+            this.startLimitValue = readFileInt(ASAHI_START_PATH);
+            if ((endValue === this.endLimitValue) && (startValue === this.startLimitValue))
                 this.emit('read-completed');
             else
                 returnError = true;
@@ -60,3 +69,4 @@ var AppleSingleBattery = GObject.registerClass({
         return status;
     }
 });
+
