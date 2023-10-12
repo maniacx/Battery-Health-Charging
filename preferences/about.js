a307720edf0705539365296d7a4d402bcabc9535
maniacx <macs18max@gmail.com>
2023-10-12 14:01:16 -0700

    Battery Health Charging: Unfortunately for Adw external link are not available in libadwaita 1.1
    Use different icon if libadwaita is 1.1 or lower

--- preferences/about.js
+++ preferences/about.js
@@ -10,6 +10,11 @@ var About = GObject.registerClass({
         'app_icon_image',
         'app_name_label',
         'version_label',
+        'readme_icon',
+        'bug_icon',
+        'translation_icon',
+        'sources_icon',
+        'rating_icon',
     ],
 }, class About extends Adw.PreferencesPage {
     constructor(window) {
@@ -20,6 +25,13 @@ var About = GObject.registerClass({
             `${Me.dir.get_child('icons').get_path()}/battery-health-charging.svg`);
         this._app_name_label.label = Me.metadata.name;
         this._version_label.label = Me.metadata.version.toString();
+
+        const linkIcon = Adw.get_minor_version() >= 2 ? 'adw-external-link-symbolic' : 'go-next-symbolic';
+        this._readme_icon.icon_name = linkIcon;
+        this._bug_icon.icon_name = linkIcon;
+        this._translation_icon.icon_name = linkIcon;
+        this._sources_icon.icon_name = linkIcon;
+        this._rating_icon.icon_name = linkIcon;
 
         // a list of actions with their associated link
         const actions = [
