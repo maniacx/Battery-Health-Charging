372c7320b5773a03f956159cf876d17ffe99b613
maniacx <macs18max@gmail.com>
2024-01-24 15:23:34 -0800

    Update installer

--- install.sh
+++ install.sh
@@ -3,10 +3,14 @@
 # Change working directory to project folder
 cd "${0%/*}"
 
+EXT_NAME="Battery Health Charging"
+EXT_UUID="Battery-Health-Charging@maniacx.github.com"
+
 if ! command -v msgfmt &> /dev/null
 then
     echo "Missing gettext!!!"
     echo "Please install gettext and re-run this installer"
+    read -n1
     exit 1
 fi
 
@@ -22,7 +26,20 @@ gnome-extensions pack ./ \
     --podir=po \
     --force \
 
-echo "Installing extension..."
-gnome-extensions install Battery-Health-Charging@maniacx.github.com.shell-extension.zip --force
+if [ $? -ne 0 ]; then 
+    echo "Error occur during compilation of Gnome Extension ${EXT_NAME}"
+    read -n1
+    exit $?
+fi
 
-echo "Extension installed Battery Health Charging succesfully. Restart the shell (or logout) to be able to enable the extension."
+echo "Installing extension..."
+gnome-extensions install $EXT_UUID.shell-extension.zip --force
+
+if [ $? -ne 0 ]; then 
+    read -n1
+    exit $?
+fi
+
+echo "Gnome Extension $EXT_NAME was succesfully installed."
+echo "Restart the shell (or logout) to be able to enable the extension."
+
