c7f07995a78a52b0ea380719068d9fb23b6addab
maniacx <macs18max@gmail.com>
2025-01-08 17:44:39 -0800

    Chromebook: Add support for cros_ec charge control driver changes for V2 systems

--- devices/Chromebook.js
+++ devices/Chromebook.js
@@ -212,4 +212,105 @@ var ChromebookSingleBattery = GObject.registerClass({
     }
 });
 
+var ChromebookSingleBatteryV2 = GObject.registerClass({
+    Signals: {'threshold-applied': {param_types: [GObject.TYPE_STRING]}},
+}, class ChromebookSingleBatteryV2 extends GObject.Object {
+    constructor(settings) {
+        super();
+        this.name = 'ChromebookV2';
+        this.type = 37;
+        this.deviceNeedRootPermission = true;
+        this.deviceHaveDualBattery = false;
+        this.deviceHaveStartThreshold = false;
+        this.deviceHaveVariableThreshold = true;
+        this.deviceHaveBalancedMode = true;
+        this.deviceHaveAdaptiveMode = false;
+        this.deviceHaveExpressMode = false;
+        this.deviceUsesModeNotValue = false;
+        this.iconForFullCapMode = '100';
+        this.iconForBalanceMode = '080';
+        this.iconForMaxLifeMode = '060';
+        this.endFullCapacityRangeMax = 100;
+        this.endFullCapacityRangeMin = 80;
+        this.endBalancedRangeMax = 85;
+        this.endBalancedRangeMin = 65;
+        this.endMaxLifeSpanRangeMax = 85;
+        this.endMaxLifeSpanRangeMin = 50;
+        this.incrementsStep = 1;
+        this.incrementsPage = 5;
+
+        this._settings = settings;
+        this.ctlPath = null;
+    }
+
+    isAvailable() {
+        if (!dmiVendor()?.includes('Google'))
+            return false;
+
+        const usesBAT0 = fileExists(BAT0_END_PATH);
+        const usesBAT1 = fileExists(BAT1_END_PATH);
+
+        if (usesBAT0 || usesBAT1) {
+            this._endPath = usesBAT0 ? BAT0_END_PATH : BAT1_END_PATH;
+            this._chromebookEndCmd = usesBAT0 ? 'BAT0_END' : 'BAT1_END';
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    async setThresholdLimit(chargingMode) {
+        this._endValue = this._settings.get_int(`current-${chargingMode}-end-threshold`);
+
+        if (this._verifyThreshold())
+            return exitCode.SUCCESS;
+
+        const [status] = await runCommandCtl(this.ctlPath, this._chromebookEndCmd, `${this._endValue}`);
+        if (status === exitCode.ERROR) {
+            this.emit('threshold-applied', 'error');
+            return exitCode.ERROR;
+        } else if (status === exitCode.TIMEOUT) {
+            this.emit('threshold-applied', 'timeout');
+            return exitCode.ERROR;
+        }
+
+        if (this._verifyThreshold())
+            return exitCode.SUCCESS;
+
+        if (this._delayReadTimeoutId)
+            GLib.source_remove(this._delayReadTimeoutId);
+        this._delayReadTimeoutId = null;
+
+        await new Promise(resolve => {
+            this._delayReadTimeoutId = GLib.timeout_add(GLib.PRIORITY_DEFAULT, 200, () => {
+                resolve();
+                return GLib.SOURCE_REMOVE;
+            });
+        });
+        this._delayReadTimeoutId = null;
+
+        if (this._verifyThreshold())
+            return exitCode.SUCCESS;
+
+        this.emit('threshold-applied', 'not-updated');
+        return exitCode.ERROR;
+    }
+
+    _verifyThreshold() {
+        this.endLimitValue = readFileInt(this._endPath);
+        if (this._endValue === this.endLimitValue) {
+            this.emit('threshold-applied', 'success');
+            return true;
+        }
+        return false;
+    }
+
+
+    destroy() {
+        if (this._delayReadTimeoutId)
+            GLib.source_remove(this._delayReadTimeoutId);
+        this._delayReadTimeoutId = null;
+    }
+});
+
 
