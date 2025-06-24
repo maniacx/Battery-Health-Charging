97437d0b45b660253511ea5029f6a8726ff8cfed
Alzemand <edilson.alzemand@codexti.com>
2025-06-23 23:19:53 -0300

    Add suporte to GalaxyBook Samsung

--- devices/Samsung.js
+++ devices/Samsung.js
@@ -4,9 +4,10 @@ import GLib from 'gi://GLib';
 import GObject from 'gi://GObject';
 import * as Helper from '../lib/helper.js';
 
-const {exitCode, fileExists, readFileInt, runCommandCtl} = Helper;
+const {exitCode, fileExists, readFileInt, runCommandCtl, writeFileInt} = Helper;
 
-const SAMSUNG_PATH = '/sys/devices/platform/samsung/battery_life_extender';
+const LEGACY_PATH = '/sys/devices/platform/samsung/battery_life_extender';
+const GALAXYBOOK_PATH = '/sys/class/power_supply/BAT1/charge_control_end_threshold';
 
 export const SamsungSingleBattery = GObject.registerClass({
     Signals: {'threshold-applied': {param_types: [GObject.TYPE_STRING]}},
@@ -26,25 +27,47 @@ export const SamsungSingleBattery = GObject.registerClass({
 
         this._settings = settings;
         this.ctlPath = null;
+
+        // Determina qual caminho está disponível
+        if (fileExists(GALAXYBOOK_PATH)) {
+            this._path = GALAXYBOOK_PATH;
+            this._mode = 'galaxybook';
+        } else if (fileExists(LEGACY_PATH)) {
+            this._path = LEGACY_PATH;
+            this._mode = 'legacy';
+        } else {
+            this._path = null;
+            this._mode = null;
+        }
     }
 
     isAvailable() {
-        if (!fileExists(SAMSUNG_PATH))
+        if (!this._path)
             return false;
         this._settings.set_int('icon-style-type', 0);
         return true;
     }
 
     async setThresholdLimit(chargingMode) {
+        if (!this._path)
+            return exitCode.ERROR;
+
         if (chargingMode === 'ful')
-            this._batteryLifeExtender = 0;
+            this._batteryLifeExtender = this._mode === 'galaxybook' ? 100 : 0;
         else if (chargingMode === 'max')
-            this._batteryLifeExtender = 1;
+            this._batteryLifeExtender = this._mode === 'galaxybook' ? 85 : 1;
 
         if (this._verifyThreshold())
             return exitCode.SUCCESS;
 
-        const [status] = await runCommandCtl(this.ctlPath, 'SAMSUNG', `${this._batteryLifeExtender}`);
+        let status;
+        if (this._mode === 'galaxybook') {
+            const success = writeFileInt(this._path, this._batteryLifeExtender);
+            status = success ? exitCode.SUCCESS : exitCode.ERROR;
+        } else {
+            [status] = await runCommandCtl(this.ctlPath, 'SAMSUNG', `${this._batteryLifeExtender}`);
+        }
+
         if (status === exitCode.ERROR) {
             this.emit('threshold-applied', 'error');
             return exitCode.ERROR;
@@ -76,8 +99,11 @@ export const SamsungSingleBattery = GObject.registerClass({
     }
 
     _verifyThreshold() {
-        const batteryLifeExtender = readFileInt(SAMSUNG_PATH);
-        this.mode = batteryLifeExtender === 1 ? 'max' : 'ful';
+        const batteryLifeExtender = readFileInt(this._path);
+        this.mode = (this._mode === 'galaxybook')
+            ? (batteryLifeExtender === 85 ? 'max' : 'ful')
+            : (batteryLifeExtender === 1 ? 'max' : 'ful');
+
         if (this._batteryLifeExtender === batteryLifeExtender) {
             this.emit('threshold-applied', 'success');
             return true;
@@ -91,4 +117,3 @@ export const SamsungSingleBattery = GObject.registerClass({
         this._delayReadTimeoutId = null;
     }
 });
-
