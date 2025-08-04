874186d5f9922f3e3e351cba6a9b8fd09d7310bb
maniacx <macs18max@gmail.com>
2025-08-04 06:22:28 -0700

    GalaxyBook: For now support both power_supply name BAT0 / BAT1

--- devices/GalaxyBook.js
+++ devices/GalaxyBook.js
@@ -7,6 +7,7 @@ const {exitCode, fileExists, readFileInt, runCommandCtl} = Helper;
 
 const VENDOR_GALAXYBOOK = '/sys/module/samsung_galaxybook';
 const VENDOR_GALAXYBOOK_MAINLINE = '/sys/class/firmware-attributes/samsung-galaxybook';
+const BAT0_END_PATH = '/sys/class/power_supply/BAT0/charge_control_end_threshold';
 const BAT1_END_PATH = '/sys/class/power_supply/BAT1/charge_control_end_threshold';
 
 export const GalaxyBookSingleBatteryBAT1 = GObject.registerClass({
@@ -14,7 +15,7 @@ export const GalaxyBookSingleBatteryBAT1 = GObject.registerClass({
 }, class GalaxyBookSingleBatteryBAT1 extends GObject.Object {
     constructor(settings) {
         super();
-        this.name = 'GalaxyBook BAT1';
+        this.name = 'GalaxyBook BAT';
         this.type = 38;
         this.deviceNeedRootPermission = true;
         this.deviceHaveDualBattery = false;
@@ -43,12 +44,18 @@ export const GalaxyBookSingleBatteryBAT1 = GObject.registerClass({
     isAvailable() {
         if (!fileExists(VENDOR_GALAXYBOOK))
             return false;
-        if (!fileExists(BAT1_END_PATH))
+        const usesBAT0 = fileExists(BAT0_END_PATH);
+        const usesBAT1 = fileExists(BAT1_END_PATH);
+
+        if (usesBAT0 || usesBAT1) {
+            this._endPath = usesBAT0 ? BAT0_END_PATH : BAT1_END_PATH;
+            this._cmd = usesBAT0 ? 'BAT0_END' : 'BAT1_END';
+        } else {
             return false;
-        if (fileExists(VENDOR_GALAXYBOOK_MAINLINE))
-            this._isOlderDrive = false;
-        else
-            this._isOlderDrive = true;
+        }
+
+        this._isOlderDrive = !!fileExists(VENDOR_GALAXYBOOK_MAINLINE);
+
         return true;
     }
 
@@ -58,7 +65,7 @@ export const GalaxyBookSingleBatteryBAT1 = GObject.registerClass({
         if (this._verifyThreshold())
             return exitCode.SUCCESS;
 
-        const [status] = await runCommandCtl(this.ctlPath, 'BAT1_END', `${this._endValue}`);
+        const [status] = await runCommandCtl(this.ctlPath, this._cmd, `${this._endValue}`);
         if (status === exitCode.ERROR) {
             this.emit('threshold-applied', 'error');
             return exitCode.ERROR;
@@ -90,7 +97,7 @@ export const GalaxyBookSingleBatteryBAT1 = GObject.registerClass({
     }
 
     _verifyThreshold() {
-        const endValue = readFileInt(BAT1_END_PATH);
+        const endValue = readFileInt(this._endPath);
         this.endLimitValue = this._isOlderDrive && endValue === 0 ? 100 : endValue;
         if (this._endValue === this.endLimitValue) {
             this.emit('threshold-applied', 'success');
